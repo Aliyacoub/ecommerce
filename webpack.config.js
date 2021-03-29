fc93e681946377d09e54c2ab581edfdc992e8145
aliyacoub <aliyacoub1996@gmail.com>
2021-03-29 23:10:44 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+  (module.exports = {
+    entry: {
+      app: './src/index.js',
+    },
+
+    output: {
+      path: path.join(__dirname, '/dist'),
+      publicPath:'',
+      filename: 'main.js',
+    },
+    mode: 'development',
+    devServer: {
+      contentBase:  path.join(__dirname, '/dist'),
+      port:1239,
+      writeToDisk: true,
+      open:true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                minimize: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+           
+            MiniCssExtractPlugin.loader, 
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [
+           
+            {
+              loader:"file-loader",
+              options:{
+                name:'[name].[ext]',
+                outputPath:"images",
+            }
+            }
+          ]
+        },
+      ],
+    },
+  
+    plugins: [
+      new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html',
+      }),
+      new  OptimizeCssAssetsPlugin({}),
+    ],
+  });
